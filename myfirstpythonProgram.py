21afc5e2f1f633db1aad35a3d4d0384421205f15
satyam <39213202+krsatyam20@users.noreply.github.com>
2019-12-02 14:42:58 +0530

    simple program

---
+++ myfirstpythonProgram.py
@@ -0,0 +1,112 @@
+a=20
+b=10
+c=a+b
+print(c)
+
+'''
+Three single coutes for 
+multiple line comment
+
+
+
+'''
+#single line comment
+'''
+
+no any data type in python like c and c++(Int float sting etc..)
+
+define varible
+a=10 : define varibale and store value
+        here a is varibale and 10 is a value
+
+a=20 : vlaue of is 20 not 10
+
+
+NOTE: indent issues in python
+
+parent
+child:not a child of above parent it`s also parent
+parent
+    child
+
+if(){
+    print("")
+    }
+
+for(){
+print() :indent issues
+}
+'''
+
+a=10
+print(type(a))
+print(a)
+
+
+a=10.12
+print(type(a))
+print(a)
+
+
+
+a="10"
+print(type(a))
+print(a)
+
+#airthmetic operator (+ - x / all bodmas)
+a=20
+b=10
+c=a+b
+print(c)
+print("add value of",a,"and",b,"=",c)
+
+
+# get input from user
+
+#use typecasting
+a=int(input("Please enter the value of a"))
+b=int(input("Please enter the value of a"))
+c=a+b
+print(c)
+print("add value of",a,"and",b,"=",c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
